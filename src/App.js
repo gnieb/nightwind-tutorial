db71d57ad5a330fa156fdab39a1cae7167c11f93
Grace Nieboer <grace.nieboer@outlook.com>
2024-03-05 21:39:47 -0500

    define all style colors with a slate palette

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ function App() {
 
 
   return (
-    <div className="w-screen h-screen flex justify-center bg-slate-200">
+    <div className="w-screen h-screen flex flex-col text-slate-800 justify-between bg-slate-200">
       <div>
         <div className="p-5 text-center text-4xl ">WELCOME TO A NIGHTWIND DEMO</div>
 
-        <div className="w-full justify-center flex">
+        <div className="w-full justify-center flex ">
           <div className="w-1/2 p-10">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            <p className="text-slate-800">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 
             </p>
           </div>
@@ -26,8 +26,9 @@ function App() {
           className="p-2 px-5 rounded-full bg-slate-800 text-white cursor-pointer"
           onClick={() => nightwind.toggle()}>FLIP THE SWITCH</div>
         </div>
-        <Footer />
+        
       </div>
+      <Footer />
     </div>
   );
 }
--- src/Footer.js
+++ src/Footer.js
@@ -1,44 +1,35 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { faStar } from '@fortawesome/free-solid-svg-icons';
-import { faFacebook } from '@fortawesome/free-brands-svg-icons';
-import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faBoltLightning } from '@fortawesome/free-solid-svg-icons';
 
 const Footer = () => {
     return (
-        <div className="sm:flex justify-between w-full text-darker font-bold bg-slate-400">
+        <div className="sm:flex justify-between w-full text-slate-200 font-bold bg-slate-500">
             <div className="p-5 sm:p-10">
-                <div>CONTACT</div>
-                <div>tylerstreesolutions@gmail.com</div>
-                <div>(585) 802 - 9632 </div>
+                <div>HELLO WORLD</div>
+                <div>dark mode is the best mode</div>
+                
             </div>
 
             <div className="p-5 sm:p-10">
-            <div>CONNECT</div>
-                <div>Want to talk trees?</div>
+            <div>CONNECT WITH ME</div>
                 <div>
-                    <a href="mailto:tylerstreesolutions@gmail.com" className="p-1 hover:text-dark"><FontAwesomeIcon icon={faEnvelope} /></a>
-                    <a href="https//:" className="p-1 hover:text-dark"><FontAwesomeIcon icon={faFacebook} /></a>
-                    <a href="tel:585-802-9632" className="p-1 hover:text-dark"><FontAwesomeIcon icon={faPhone}/></a>
+                    <a href="mailto:grace.nieboer@outlook.com" className="p-1 hover:text-white"><FontAwesomeIcon icon={faEnvelope} /></a>
+                    <a href="https://github.com/gnieb" className="p-1 hover:text-dark"><FontAwesomeIcon icon={faGithub} /></a>
+                    <a href="https://www.linkedin.com/in/gracenieboer/" className="p-1 hover:text-dark"><FontAwesomeIcon icon={faLinkedin}/></a>
                 
                 </div>
             </div>
 
             <div className="p-5 sm:p-10">
-                <div>5.0</div>
-                
-                <div>
-                    <FontAwesomeIcon icon={faStar} />
-                    <FontAwesomeIcon icon={faStar} />
-                    <FontAwesomeIcon icon={faStar} />
-                    <FontAwesomeIcon icon={faStar} />
-                    <FontAwesomeIcon icon={faStar} />
-                </div>
-                <div className="font-bold hover:text-dark">
-                    <a href="https://www.google.com/search?q=tylers+tree+solutions&rlz=1C5CHFA_enUS899US899&oq=tylers&gs_lcrp=EgZjaHJvbWUqBggBEEUYOzIMCAAQRRg5GLEDGIAEMgYIARBFGDsyCQgCEAAYChiABDIJCAMQABgKGIAEMgcIBBAAGIAEMgYIBRBFGD0yBggGEEUYPDIGCAcQRRg80gEIMzc0NmowajeoAgCwAgA&sourceid=chrome&ie=UTF-8#lrd=0x89d153a2289fe4e9:0xb8b8532813616cc2,3,,,," 
-                        target="_blank"
-                        rel="noopener">
-                        GREAT EXPERIENCE? LEAVE A REVIEW</a>
+               
+                    <div>WANT MORE?</div>
+             
+                <div className="font-bold hover:text-white">
+                    <a href="https://gracenieboer.netlify.app/">PORTFOLIO<FontAwesomeIcon icon={faBoltLightning}/></a>
                 </div>
 
             </div>
